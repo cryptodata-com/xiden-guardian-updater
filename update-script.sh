1d3de64370db6bcc27ca9fbd7d748bfa86c9eb13
dclaudiud <50233600+dclaudiud@users.noreply.github.com>
2022-02-28 16:10:53 +0200

    Update update-script.sh

--- update-script.sh
+++ update-script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # /usr/bin/wget -O - "https://raw.githubusercontent.com/cryptodata-com/xiden-guardian-updater/master/opkg-upgrade.sh" | /bin/ash
-echo "Updating"
+echo "Updating\n" >> /root/update.log
