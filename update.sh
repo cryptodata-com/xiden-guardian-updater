c102c13701b681476033c98cecc45ed3015abdb6
Andrei Buiu <andrei@buiu.co.uk>
2022-02-28 16:20:00 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/bin/wget -O - "https://raw.githubusercontent.com/cryptodata-com/xiden-guardian-updater/master/update-script.sh | /bin/ash
+/usr/bin/wget -O - "https://raw.githubusercontent.com/cryptodata-com/xiden-guardian-updater/master/update-script.sh" | /bin/ash
